5a605a523206c95da271fc13ee66707ee74706a9
Nasir-Raza <send2raza@gmail.com>
2022-07-04 17:19:16 +0500

    Third commit to JS-Assignment-12 Custom carousel control by adding color animation

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 var imageList = ["mountain.jpg", "forest.jpg", "lake.jpg", "nature.jpg", "ocean.jpg", "lights.jpg", "mountains.jpg", "valley.jpg", "snow.jpg"];
+var colorList = ["#717171", "#1e90ff", "#f7260a", "#0e11e6", "#f0dd11", "#09e9ed", "#bd08b4", "#faa405", "#63041c", "#062b18"];
 var count = 0;
 var dom = document.getElementById("banners");
 var dom1 = document.getElementById("dotbtn");
 var dots = document.getElementsByClassName("dot");
+// Get the root element
+var r = document.querySelector(':root');
 
 function currentSlide(n, isDoted) {
   for (var i = 0; i < dots.length; i++) {
@@ -30,6 +33,7 @@ function bannersInit(index) {
     dots[index].className += " active";
   }
   count++;
+  dotColorChange();
 }
 currentSlide(count, false);
 
@@ -49,4 +53,21 @@ function preNextSlide(ind) {
     count = imageList.length - 1;
   }
   currentSlide(count, false);
+}
+
+// Create a function for setting variable values
+function dotColorChange() {
+  // Random color pick between 0 and max i.e. 0 and 9
+  var randomColorDot = "";
+  var randomColorPreNext = "";
+  var max = 9;
+  var indexNumDot = Math.floor(Math.random() * (max + 1));
+  var indexNumPreNext = Math.floor(Math.random() * (max + 1));
+
+  randomColorDot = colorList[indexNumDot];
+  randomColorPreNext = colorList[indexNumPreNext];
+
+  // Set the value of variable --gray and --white to another randomly generated values
+  r.style.setProperty('--gray', randomColorDot);
+  r.style.setProperty('--white', randomColorPreNext);
 }
